8090cd318d0c3471e8b98ea3173213fa152e46ff
Robin <robin.van.den.eerenbeemd@gmail.com>
2025-08-10 16:44:48 +0100

    update buttonbar to NCCA Coding standards

--- include/buttonBar.h
+++ include/buttonBar.h
@@ -1,3 +1,10 @@
+//header file encapulates the button bar class, which is used to manage the buttons in the script editor tab
+//author: Robin van den Eerenbeemd
+//version: 1.0
+//date: 10 August 2025 updated to NCCA coding standards
+
+
+
 #pragma once
 
 #include <QWidget>
@@ -21,13 +28,14 @@ class ButtonBar : public QWidget
     Q_OBJECT
 
 public:
+/// Constructor for the ButtonBar class
 explicit ButtonBar(TabScriptEditor* tabEditor, Console *console, QWidget *parent = nullptr);
     ~ButtonBar();
 
 
 private slots:
 
-
+    // Slot functions for button clicks
     void on_saveButton_clicked();
 
     void on_clearButton_clicked();
@@ -43,13 +51,19 @@ private slots:
     void on_clearTerminalButton_clicked();
 
 private:
+    // Private member functions
     Ui::ButtonBar *ui;
+
+    // pointers to classes used in the button bar
     EditFile *editfile;
     SearchAndReplace *searchandreplace;
     OpenTemplate *opentemplate;
     QTabWidget* tabWidget;
-    CodeEditor* currentEditor() const;
     TabScriptEditor* tabEditor;
     Console *console;
+
+    // Function to get the current editor from the tab editor
+    CodeEditor* currentEditor() const;
+
 };
 
